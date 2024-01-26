c7d4a0b9b043eda7038934d989d5c32d959f7530
rampsad27 <sainamaskey27@gmail.com>
2024-01-26 17:02:49 +0545

    loginn+feedd+data

---
+++ lib/ui/modules/post_modules.dart
@@ -0,0 +1,13 @@
+// class PostModel {
+//   final String postedby;
+//   final String likes;
+//   final String comments;
+//   final String imageUrl;
+
+//   PostModel({
+//     required this.postedby,
+//     required this.likes,
+//     required this.comments,
+//     required this.imageUrl,
+//   });
+// }
--- lib/ui/modules/widgets/posts_feed.dart
+++ lib/ui/modules/widgets/posts_feed.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import 'package:ig/ui/modules/screen/story.dart';
+import 'package:ig/ui/modules/widgets/story_design.dart';
 // import 'package:ig/ui/modules/widgets/line.dart';
 
 class PostsFeed extends StatefulWidget {
@@ -14,105 +17,148 @@ class _PostsFeedState extends State<PostsFeed> {
   int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 120,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 69,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                        builder: (context) => const StoryScreen()),
-                  );
-                },
-                child: Container(
-                  margin: const EdgeInsets.only(left: 8),
-                  child: const CircleAvatar(
-                    radius: 40,
-                    backgroundColor: Color.fromARGB(255, 230, 172, 110),
-                  ),
-                  // child: CircleAvatar(
-                  //     radius: 39,
-                  //     Image.network(
-                  //         "https://images.unsplash.com/photo-1706067003003-f5d8d518c00b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D"),
-                  //         )),
-                ),
-              );
-            },
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 90,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 69,
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                          builder: (context) => const StoryScreen()),
+                    );
+                  },
+                  child: const Storydesign(),
+                );
+              },
+            ),
           ),
         ),
-        // const HorizontalLineWithText(),
-        Expanded(
-          child: ListView.builder(
-            itemCount: widget.postModelList.length,
-            itemBuilder: (context, index) {
-              return InkWell(
-                onTap: () {
-                  setState(
-                    () {
-                      selectedIndex = index;
-                    },
-                  );
-                },
-                child: Container(
-                  height: 330,
-                  margin: const EdgeInsets.only(bottom: 30),
-                  // color: const Color.fromARGB(255, 131, 170, 224),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        // posts
+        SliverList.builder(
+          itemCount: widget.postModelList.length,
+          itemBuilder: (context, index) {
+            return Container(
+              height: 600,
+              margin: const EdgeInsets.only(bottom: 10),
+              // color: const Color.fromARGB(255, 131, 170, 224),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                    child: Row(
                       children: [
-                        Text(
-                          widget.postModelList[index].postedby,
-                          style: const TextStyle(
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold,
-                          ),
+                        CircleAvatar(
+                          radius: 18,
+                          backgroundImage: NetworkImage(
+                              widget.postModelList[index].imagecUrl),
                         ),
-                        Image.network(
-                          widget.postModelList[index].imageUrl,
-                          width: double.infinity,
-                          height: 198,
-                          fit: BoxFit.cover,
-                        ),
-                        const SizedBox(height: 10),
-                        const Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        const SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Icon(CupertinoIcons.heart),
-                            SizedBox(width: 8),
-                            Icon(Icons.comment_rounded), SizedBox(width: 8),
-                            Icon(Icons.send),
-                            Spacer(), // Spacer widget takes up any remaining space
-                            Icon(Icons.bookmark_border_outlined),
+                            Text(
+                              widget.postModelList[index].postedby,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              widget.postModelList[index].location,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            // const Text(
+                            //   "Sound Text",
+                            //   style: TextStyle(
+                            //     fontSize: 14,
+                            //     color: Colors.grey,
+                            //   ),
+                            // ),
                           ],
                         ),
                         const Spacer(),
+                        const Icon(Icons.more_horiz_outlined),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  AspectRatio(
+                    aspectRatio: 1.0,
+                    child: Image.network(
+                      widget.postModelList[index].imageUrl,
+                      width: double.infinity,
+                      // height: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  // const SizedBox(height: 10),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Row(
+                          children: [
+                            Icon(CupertinoIcons.heart),
+                            SizedBox(width: 8),
+                            Icon(Icons.comment_outlined),
+                            SizedBox(width: 8),
+                            Icon(Icons.send),
+                            Spacer(),
+                            Icon(Icons.bookmark_border_outlined),
+                          ],
+                        ),
+                        const SizedBox(height: 8),
                         Text(
                           " ${widget.postModelList[index].likes} likes",
                           style: const TextStyle(
-                            fontSize: 18,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
                           ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              widget.postModelList[index].postedby,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            Text(
+                              widget.postModelList[index].caption,
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
                         ),
                         Text(
                           " View all ${widget.postModelList[index].comments} comments",
                           style: const TextStyle(
-                            fontSize: 16,
+                            fontSize: 14,
                             color: Color.fromARGB(255, 156, 156, 156),
                           ),
                         ),
                       ],
                     ),
                   ),
-                ),
-              );
-            },
-          ),
+                ],
+              ),
+            );
+          },
         ),
       ],
     );
@@ -121,14 +167,20 @@ class _PostsFeedState extends State<PostsFeed> {
 
 class PostModel {
   final String postedby;
+  final String location;
   final String likes;
+  final String caption;
   final String comments;
   final String imageUrl;
+  final String imagecUrl;
 
   PostModel({
     required this.postedby,
+    required this.location,
     required this.likes,
+    required this.caption,
     required this.comments,
     required this.imageUrl,
+    required this.imagecUrl,
   });
 }
