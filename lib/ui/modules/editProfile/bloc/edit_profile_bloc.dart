95a49cfdf4f1590f0d473f25d62d240b56b1ff40
rampsad27 <sainamaskey27@gmail.com>
2024-02-13 12:42:11 +0545

    ig+theme+authh-save+ppImg-lognout+namecopy+liks

--- lib/ui/modules/editProfile/bloc/edit_profile_bloc.dart
+++ lib/ui/modules/editProfile/bloc/edit_profile_bloc.dart
@@ -11,15 +11,15 @@ class EditProfileBloc extends Bloc<EditProfileEvent, EditProfileState> {
   EditProfileBloc({required this.profileRepository})
       : super(EditProfileDataInitial()) {
     on<EditProfileDataRequested>((event, emit) async {
-      emit(EditProfileDataProgress());
-      await Future.delayed(const Duration(seconds: 2));
+      // emit(EditProfileDataProgress());
+      // await Future.delayed(const Duration(seconds: 2));
       await profileRepository.saveUserProfileInfo(event.name, event.username);
       add(CheckEditProfileData());
       // add(EditProfileDataRequested());
     });
     on<CheckEditProfileData>((event, emit) async {
       emit(EditProfileDataProgress());
-      await Future.delayed(const Duration(seconds: 2));
+      await Future.delayed(const Duration(seconds: 1));
       // bool isLoggedIn = await authenticationRepository.isUserLoggedIn();
       var temp = profileRepository.getUserProfileInfo();
       emit(EditProfileDataSuccess(name: temp.name));
--- lib/ui/modules/widgets/posts_feed.dart
+++ lib/ui/modules/widgets/posts_feed.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ig/ui/modules/bloc/feed_bloc.dart';
-
 import 'package:ig/ui/modules/components/model.dart';
+import 'package:ig/ui/modules/likesbloc/feed_bloc.dart';
 import 'package:ig/ui/modules/widgets/bottomsheet.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 import 'package:readmore/readmore.dart';
